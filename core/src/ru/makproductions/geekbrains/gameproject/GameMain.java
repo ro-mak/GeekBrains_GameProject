d80f47ebfd387a1ed0bf40c484b52c81ff3dadd3
ro-mak <eee127@yandex.ru>
2017-08-15 22:34:12 +0300

    Just having fun

--- core/src/ru/makproductions/geekbrains/gameproject/GameMain.java
+++ core/src/ru/makproductions/geekbrains/gameproject/GameMain.java
@@ -9,11 +9,16 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class GameMain extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	Texture img2;
+	float x;
+	float y;
+	float i = 1;
+	float j = 1;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("GeekBackGround.png");
+		img2 = new Texture("badlogic.jpg");
 	}
 
 	@Override
@@ -22,7 +27,33 @@ public class GameMain extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		bounceBadlogicImage();
 		batch.end();
+	}
+
+	private void bounceBadlogicImage(){
+
+		if(x < 0){
+			x = 0;
+			i = 1;
+		}
+		if(x > 200){
+			x = 200;
+			i = -1;
+			System.out.println(i);
+		}
+		if(y > 200){
+			x = 200;
+			j = -1;
+			System.out.println(j);
+		}
+		if(y < 0){
+			y = 0;
+			j = 1;
+		}
+		batch.draw(img2,x,y);
+		x+=i;
+		y+=j;
 	}
 	
 	@Override
