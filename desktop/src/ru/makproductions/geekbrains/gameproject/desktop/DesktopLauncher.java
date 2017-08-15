61282be1ab22eb7e64cce30aa0e4731811ca8713
ro-mak <eee127@yandex.ru>
2017-08-15 21:49:55 +0300

    Created a LibGDX project

---
+++ desktop/src/ru/makproductions/geekbrains/gameproject/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.makproductions.geekbrains.gameproject.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.makproductions.geekbrains.gameproject.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
